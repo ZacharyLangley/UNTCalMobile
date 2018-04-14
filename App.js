414d224d17bb76177b1c5005abec8787b25a3de6
Zachary L <zacharylangley@my.unt.edu>
2018-04-14 15:14:34 -0500

    store and reducers

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './src/reducers/index';
+
 // Components
 import Main from './src/components/Main';
 
 export default class App extends Component {
   render() {
+
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <View style={styles.container}>
-        <Main />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Main />
+        </View>
+      </Provider>
     );
   }
 }
